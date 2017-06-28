e361e295059fd342561c747ef59576f63593e52c
zhangxinxu <zhangxinxu@zhangxinxu.com>
2017-06-28 10:44:02 +0800

    add ga
    add ga

--- renderer.js
+++ renderer.js
@@ -1353,7 +1353,7 @@ var MenuItem = remote.MenuItem;
 					    	// 海外版，使用ga统计
 					    	self.log(filename + ': 正在写入ga统计...');
 
-					    	insertHTML = '<script>/* ga统计 */</script>';
+					    	insertHTML = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-93025844-1', 'auto');ga('send', 'pageview');</script>";
 
 					    	// 同时lang标记为en
 					    	data = data.replace('<html>', '<html lang="en">');
